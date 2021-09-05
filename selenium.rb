091a29e8840e598812106411954c56d12dc3ef2e
Froz10 <ivan.frozby@gmail.com>
2021-09-05 23:44:52 +0600

    First parse

--- selenium.rb
+++ selenium.rb
@@ -1,13 +1,44 @@
 require 'selenium-webdriver'
+require 'uri'
 
-driver = Selenium::WebDriver.for :firefox
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
+def init_selenium_webdriver
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  return Selenium::WebDriver.for :chrome, options: options
+end
 
-begin
-  driver.get 'https://google.com/ncr'
-  driver.find_element(name: 'q').send_keys 'cheese', :return
-  first_result = wait.until { driver.find_element(css: 'h3') }
-  puts first_result.attribute('textContent')
-ensure
+def navigate(driver, url, page_no)
+  driver.navigate.to URI.join(url, page_no.to_s)
+end
+
+def get_data(driver, pattern)
+  return driver.find_elements(pattern)
+end
+
+def has_data?(data)
+  return data.size() > 0
+end
+
+def parse_data(data, pattern)
+  data.each do |quote|
+    text = quote.find_element(pattern[:text]).text
+    author = quote.find_element(pattern[:author]).text
+    puts "#{text} -- #{author}"
+  end
+end
+
+def main
+  driver = init_selenium_webdriver
+  url = "http://quotes.toscrape.com/page/"
+  page_no = 1
+  loop do
+    navigate(driver, url, page_no)
+    data = get_data(driver, {class_name: 'quote'})
+    parse_data(data, {text: {class_name: 'text'}, author: {class_name: 'author'}})
+    page_no += 1
+    break unless has_data?(data)
+  end
   driver.quit
 end
+
+main
